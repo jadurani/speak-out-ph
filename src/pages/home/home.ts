f9de21a239b9082b0ddd6cffe70f4f8b026f6f53
Jadurani Davalos <jadurani.davalos@gmail.com>
2017-11-26 03:23:09 +0800

    Mocked data on home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {
+  NavController,
+  ActionSheetController
+} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public actionSheetCtrl: ActionSheetController
+  ) {
 
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select message',
+      buttons: [
+        {
+          text: 'Blah 1',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: 'Blah 2',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
